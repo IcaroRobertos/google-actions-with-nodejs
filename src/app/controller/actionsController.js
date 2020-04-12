1f5ceba5489d0cb86d7fcf735c62ffebd6c5ae73
Icaro Roberto <oi@icaroroberto.com>
2020-04-12 02:05:53 -0300

    Init Actions Controller

---
+++ src/app/controller/actionsController.js
@@ -0,0 +1,5 @@
+const { dialogflow } = require("actions-on-google");
+
+const app = dialogflow({ debug: true });
+
+module.exports = app;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 const routes = require("express").Router();
 
+const ActionsController = require("./app/controller/actionsController");
+
 routes.get("/", (req, res) => {
   res.send({ ok: true });
 });
+
+routes.post("/", ActionsController);
 
 module.exports = routes;
