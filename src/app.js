1d59e21ae842fdab253d6640662b47c9247e262b
Icaro Roberto <oi@icaroroberto.com>
2020-04-12 02:02:07 -0300

    Init Class App to Start Server

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const routes = require("./routes");
+
+class App {
+  constructor() {
+    this.express = express();
+
+    this.middleware();
+    this.routes();
+  }
+
+  middleware() {
+    this.express.use(cors());
+    this.express.use(bodyParser.json());
+  }
+
+  routes() {
+    this.express.use(routes);
+  }
+}
+
+module.exports = new App().express;
