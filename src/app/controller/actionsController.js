fa52809f988250a253b990dcdc669783e59d3bc3
Icaro Roberto <oi@icaroroberto.com>
2020-04-12 02:10:53 -0300

    Finish Demo

--- src/app/controller/actionsController.js
+++ src/app/controller/actionsController.js
@@ -2,4 +2,9 @@ const { dialogflow } = require("actions-on-google");
 
 const app = dialogflow({ debug: true });
 
+app.intent("Default Welcome Intent", (conv) => {
+  conv.add("Oi, Eu me chamo Ícaro Roberto.");
+  conv.ask("Em que posso te ajudar?");
+});
+
 module.exports = app;
